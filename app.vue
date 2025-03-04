649e45adc63dbf882d7722a097748d8dd49e2255
psantos165 <psantos_165@hotmail.com>
2025-03-04 16:27:30 -0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <!-- preambulo - insruções para o brownser/sistema de busca --> 
+  <meta charset="UTF-8"> <!-- pacote de simbolos -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Pri Website</title>
+  <!-- CSS é um recurso critico para renderização -->
+  <link
+  href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+  rel="stylesheet"
+  />
+</head>
+<body class="bg-gray-400">
+  <figure class="p-6">
+    <img
+      class="w-32 h-32 rounded-full mx-auto"
+      src="https://placehold.co/400x400"
+      alt="profile picture"
+      width="400"
+      height="400"
+    />
+    <div class="pt-6 text-center space-y-4">
+      <figcaption class="font-medium">
+        <div class="text-cyan-900 text-xl">Priscila Santos</div>
+        <div class="text-gray-500 font-light">@psantos_165</div>
+      </figcaption>
+      <blockquote>
+        <p class="text-lg font-medium">Telecom Infrastructure | Leader</p>
+      </blockquote>
+    </div>
+  </figure>
+   <!-- Header -->
+   <main class="max-w-xs mx-auto">
+     <!-- First Button-->
+  <div class="pt-6 text-center space-y-4">
+  <a
+    href="https://wa.me/+5511998574402"
+    target="_blank"
+    class="flex rounded-lg border border-gray-400 bg-white px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150"
+  >
+    <p class="mr-3 h-6 w-6">☎️</p>
+    WhatsApp
+    <div class="ml-auto mt-0.5 pl-4">
+      <svg
+        class="h-5 w-5 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+    </div>
+  </a>
+</div>
+<!-- First Button-->
+<!-- Second Button -->
+<div class="pt-6 text-center space-y-4">
+  <a
+    href="https://www.linkedin.com/in/priscila-santos-a30a9b13/"
+    target="_blank"
+    class="flex rounded-lg border border-gray-400 bg-white px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150"
+  >
+    <p class="mr-3 h-6 w-6">👩‍💻</p>
+    Linkedin
+    <div class="ml-auto mt-0.5 pl-4">
+      <svg
+        class="h-5 w-5 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+    </div>
+  </a>
+</div>
+<!-- ./Second Button -->
+ <!-- Third Button -->
+ <div class="pt-6 text-center space-y-4">
+  <a
+    href="https://github.com/psantos165"
+    target="_blank"
+    class="flex rounded-lg border border-gray-400 bg-white px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150"
+  >
+    <p class="mr-3 h-6 w-6">🛠️</p>
+    Github
+    <div class="ml-auto mt-0.5 pl-4">
+      <svg
+        class="h-5 w-5 text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+        />
+      </svg>
+    </div>
+  </a>
+</div>
+<!-- ./Third Button -->
+<!-- Fourth Button -->
+<div class="pt-6 text-center space-y-4">
+<a
+  href="https://www.instagram.com/psantos_165/"
+  target="_blank"
+  class="flex rounded-lg border border-gray-400 bg-white px-5 py-4 text-lg leading-6 font-medium shadow-md hover:shadow-xl transition ease-in-out duration-150"
+>
+  <p class="mr-3 h-6 w-6">📷</p>
+  Instagram
+  <div class="ml-auto mt-0.5 pl-4">
+    <svg
+      class="h-5 w-5 text-gray-400"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+      />
+    </svg>
+  </div>
+</a>
+</div>
+<!-- ./Fourth Button -->
+</main>
+</body>
+</html>
+  </div>
+</template>
